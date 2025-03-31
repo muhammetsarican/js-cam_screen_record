1224dcae7629297a75e1131413554fa686f33e65
Muhammet Sarıcan <muhammetsarican@gmail.com>
2025-03-31 14:24:26 +0300

    refactor: demo 2 side main structure created.

--- demo-2/src/assets/js/recorder-class.js
+++ demo-2/src/assets/js/recorder-class.js
@@ -1,41 +1,77 @@
+const button = document.querySelector("#start-record");
+const cam_video = document.querySelector("#cam-stream");
+
 class ExamRecorder {
     cam_stream;
     screen_stream;
 
     recorder;
 
-    setStreamsAndRecorder() {
+    socket;
+
+    constructor() {
+        this.socket = new WebSocket("ws://localhost:3002");
+    }
+
+    async setStreamsAndRecorder() {
         const options = {
             video: true,
             audio: true
         };
-        navigator.mediaDevices.getUserMedia(options)
-            .then((stream) => {
-                this.cam_stream = stream;
-            })
-            .catch(err => console.log(err));
-
-        navigator.mediaDevices.getDisplayMedia({
+        this.cam_stream = await navigator.mediaDevices.getUserMedia({
             ...options,
-            preferCurrentTab: true
-        })
-            .then((stream) => {
-                this.screen_stream = stream;
-            })
-            .catch(err => console.log(err));
+            audio: false
+        });
 
-        this.recorder = new MediaRecorder([this.cam_stream, this.screen_stream]);
+        this.screen_stream = await navigator.mediaDevices.getDisplayMedia(options);
+
+        this.recorder = new MediaRecorder(this.cam_stream);
+        console.log(this.recorder);
     }
 
-    startRecording() {
+    async start() {
         console.log("record started")
-        this.setStreamsAndRecorder();
-        this.recorder.startRecording();
+        await this.setStreamsAndRecorder();
+        this.recorder.start();
+        cam_video.srcObject = this.cam_stream;
+        cam_video.onloadedmetadata = () => {
+            cam_video.play();
+        }
     }
 
+    stop() {
+        return this.recorder.stop();
+    }
+
+    sendRecorderData() {
+        this.recorder.ondataavaliable = (e) => {
+            console.log("the data is available");
+
+            // sending socket the record data
+            this.socket.send(e.data);
+        };
+    }
+
+    sendMessage() {
+        // Get the value of
+        // the message input
+        const message = "Hello dear user";
+        // Send the message to 
+        // the WebSocket server
+        this.socket.send(message);
+        // Clear the message input
+        messageInput.value = '';
+    }
 }
 
-const button = document.querySelector("#start-record");
 const recorder = new ExamRecorder();
 
-button.addEventListener("click", () => recorder.startRecording());
+button.addEventListener("click", async () => {
+    await recorder.start();
+    const sleep = () => new Promise((resolve, reject) => {
+        setTimeout(resolve, 5000);
+    });
+    await sleep();
+    await recorder.stop();
+    console.log("record stopped");
+});
